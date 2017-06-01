9c98a5a7c3bb06fec6c4a215b166283ae7dfa483
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-06-01 13:00:10 -0400

    funcionalidad js y gryd system

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ (function main(){
+	var boxes = Array.from(document.getElementsByClassName("imagenes"));
+	var work = document.getElementById("work");
+
+	var modal, bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal = document.createElement("div");
+			modal.classList.add("box-services-modal");
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "assets/images/close.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			work.appendChild(modal);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
